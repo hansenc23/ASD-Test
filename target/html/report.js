d0ef3074d03d8850bb963f05a4b192edfd8a7dee
nxhieu <41371059+nxhieu@users.noreply.github.com>
2019-09-14 18:24:59 +1000

    add topup

--- target/html/report.js
+++ target/html/report.js
@@ -1,4 +1,280 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:opalFeatures/UserRegistrationFeature.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:opalFeatures/TopUpManagementFeature.feature");
+formatter.feature({
+  "name": "TopUpManagementFeature",
+  "description": "    As a user of Opal project \n    I want to create/delete/update payment method\n    So that I can top up my card with my payment method",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Successfully create/update/delete preffered payment method",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User is in the \"login.jsp\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_is_in_the(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User fill in user detail",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_fill_in_user_detail()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"login_submit\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"loginAction_success\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"sidebar_paymentMethodManagement\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"paymentMethodManagement_addMethod\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User fill in paymentmethod detail",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_fill_in_paymentmethod_detail()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"addPaymentMethod_submit\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"addPaymentMethodOutcome_back\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"9876987698769876\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User fill in update detail",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_fill_in_update_detail()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"updatePaymentmethod_update\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"updatePaymentmethod_back\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"7654765476547654\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"updatePaymentmethod_delete\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"updatePaymentmethod_back\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Successfully create top up card",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User is in the \"login.jsp\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_is_in_the(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User fill in user detail",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_fill_in_user_detail()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"login_submit\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"loginAction_success\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"sidebar_topUpValue\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User fill in payment amount",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_fill_in_payment_amount()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"topUpValue_submit\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User fill in the payment detail",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_fill_in_the_payment_detail()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"topUpCard_submit\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click \"topUpConfirmation_confirm\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TopUpManagementFeature.user_click_loginsuccess(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.uri("file:opalFeatures/UserRegistrationFeature.feature");
 formatter.feature({
   "name": "Online User Registration",
   "description": "",
@@ -14,7 +290,7 @@ formatter.step({
   "keyword": "Given "
 });
 formatter.match({
-  "location": "RegisterFeature.user_is_in_the(String)"
+  "location": "TopUpManagementFeature.user_is_in_the(String)"
 });
 formatter.result({
   "status": "passed"
@@ -23,31 +299,25 @@ formatter.step({
   "name": "User Click on \"navbar_register\"",
   "keyword": "When "
 });
-formatter.match({
-  "location": "RegisterFeature.user_Click_on(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "User fill in contact detail",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_fill_in_contact_detail()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "User clicks \"back\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_Clicks(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "name": "Successfully update account details",
@@ -58,131 +328,105 @@ formatter.step({
   "name": "user is in \"login.jsp\"",
   "keyword": "Given "
 });
-formatter.match({
-  "location": "RegisterFeature.user_is_in(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user fill in login details",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_fill_in_details()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user choose \"loginAction_success\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_click(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "User Clicks on \"navbar_profile\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_Clicks_on(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "User edits \"edit_profile\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_edits(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "User update details",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_update_details()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user click to \"back_to_profile\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_click_to(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user click change password \"change_password\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_click_change_password(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user update password",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_update_password()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user is back to profile \"back_to_profile\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_back_to_profile(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user choose add security question \"add_question\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_add_security_question(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user choose from a list of question",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_choose_question()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user goes back to profile \"back_to_profile\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_goes_to_profile(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "name": "Successfully forget password",
@@ -193,40 +437,32 @@ formatter.step({
   "name": "user in \"login.jsp\"",
   "keyword": "Given "
 });
-formatter.match({
-  "location": "RegisterFeature.user_in(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user chooses \"forget_button\"",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_chooses(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user enter email address",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_enter_email_address()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user input answer",
   "keyword": "And "
 });
-formatter.match({
-  "location": "RegisterFeature.user_input_answer()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 });
