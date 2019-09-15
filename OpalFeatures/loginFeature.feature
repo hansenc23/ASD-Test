1bc2fd957166058f3ff6d9d68496e88c86bb26e0
Hansen Christian <13052434@student.uts.edu.au>
2019-09-15 16:59:36 +1000

    update register testing and change minor details

--- OpalFeatures/loginFeature.feature
+++ OpalFeatures/loginFeature.feature
@@ -1,7 +1,7 @@
 #----------------------------------
 # Empty Cucumber .feature file
 #----------------------------------
-   Feature: Access Manager
+#   Feature: Access Manager
 
 # Scenario: A user will be able to login , and logout
 # Given User is in the "main.jsp"
@@ -13,13 +13,13 @@
 #     Then The page should be navigated to "logout.jsp"
 
    
-Scenario: A user want to delete their access history 
-Given User is in the "main.jsp"
-    When User Click on "navbar_login"
-    And User fill user information
-    And User Click on "login_submit"
-    And User Click on "loginAction_success"
-    And User Click "sidebar_accessManager"
-    And User Click on "delete"
-    And User refresh page
-    Then The page should be navigated to "Access manager"
+# Scenario: A user want to delete their access history 
+# Given User is in the "main.jsp"
+#     When User Click on "navbar_login"
+#     And User fill user information
+#     And User Click on "login_submit"
+#     And User Click on "loginAction_success"
+#     And User Click "sidebar_accessManager"
+#     And User Click on "delete"
+#     And User refresh page
+#     Then The page should be navigated to "Access manager"
--- target/html/report.js
+++ target/html/report.js
@@ -1,100 +1,205 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:opalFeatures/loginFeature.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:opalFeatures/UserRegistrationFeature.feature");
 formatter.feature({
-  "name": "Access Manager",
+  "name": "Online User Registration",
   "description": "",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "A user want to delete their access history",
+  "name": "Successfully update account details",
   "description": "",
   "keyword": "Scenario"
 });
 formatter.step({
-  "name": "User is in the \"main.jsp\"",
+  "name": "user is in \"login.jsp\"",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "loginFeature.user_is_in_the(String)"
+  "location": "RegisterFeature.user_is_in(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User Click on \"navbar_login\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "loginFeature.user_Click_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User fill user information",
+  "name": "user fill in login details",
   "keyword": "And "
 });
 formatter.match({
-  "location": "loginFeature.user_fill_user_information()"
+  "location": "RegisterFeature.user_fill_in_details()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User Click on \"login_submit\"",
+  "name": "user choose \"loginAction_success\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "loginFeature.user_Click_on(String)"
+  "location": "RegisterFeature.user_click(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User Click on \"loginAction_success\"",
+  "name": "User Clicks on \"navbar_profile\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "loginFeature.user_Click_on(String)"
+  "location": "RegisterFeature.user_Clicks_on(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User Click \"sidebar_accessManager\"",
+  "name": "User edits \"edit_profile\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "loginFeature.user_Click(String)"
+  "location": "RegisterFeature.user_edits(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User Click on \"delete\"",
+  "name": "User update details",
   "keyword": "And "
 });
 formatter.match({
-  "location": "loginFeature.user_Click_on(String)"
+  "location": "RegisterFeature.user_update_details()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User refresh page",
+  "name": "user click to \"back_to_profile\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "loginFeature.user_refresh_page()"
+  "location": "RegisterFeature.user_click_to(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "The page should be navigated to \"Access manager\"",
+  "name": "user click change password \"change_password\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_click_change_password(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user update password",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_update_password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is back to profile \"back_to_profile\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_back_to_profile(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user choose add security question \"add_question\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_add_security_question(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user choose from a list of question",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_choose_question()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user goes back to profile \"back_to_profile\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_goes_to_profile(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should be directed to \"Profile\"",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "loginFeature.the_page_should_be_navigated_to(String)"
+  "location": "RegisterFeature.user_should_be_directed_to(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Successfully forget password",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user in \"login.jsp\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "RegisterFeature.user_in(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user chooses \"forget_button\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_chooses(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enter email address",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_enter_email_address()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user input answer",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "RegisterFeature.user_input_answer()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "page should be directed to \"Forget Password\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "RegisterFeature.page_should_be_directed_to(String)"
 });
 formatter.result({
   "status": "passed"
