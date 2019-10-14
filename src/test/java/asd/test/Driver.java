32c8d62ba5bd8a3a88c781f60ac77565cd296e1f
HC <HC@172.19.117.164>
2019-10-14 15:59:16 +1100

    test linux chromedriver

--- src/test/java/asd/test/Driver.java
+++ src/test/java/asd/test/Driver.java
@@ -1,27 +1,27 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package asd.test;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-///**
-// *
-// * @author aasdd
-// */
-//public class Driver {
-//    
-//    protected WebDriver driver;
-//    
-//    protected WebDriver getDriver(){
-//        if(driver == null){ 
-//            System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//chromedriver//chromedriver.exe");
-//            driver = new ChromeDriver();
-//        }
-//        return driver;
-//    }
-//    
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asd.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author aasdd
+ */
+public class Driver {
+    
+    protected WebDriver driver;
+    
+    protected WebDriver getDriver(){
+        if(driver == null){ 
+            System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//chromedriver//chromedriver");
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+    
+}
