2f6f884f765792e7539d5e4be7a36ebdad38aced
nxhieu <41371059+nxhieu@users.noreply.github.com>
2019-10-17 03:48:22 +1100

    test hieu feature

--- target/html/report.js
+++ target/html/report.js
@@ -1,460 +1,195 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:OpalFeatures/TransferBalanceFeature.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:OpalFeatures/TransportNewsManagement.feature");
 formatter.feature({
-  "name": "Transfer Balance Management",
+  "name": "Transport News Management",
   "description": "",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "Successfully transfer balance",
+  "name": "Successfuly create, edit, delete an Article",
   "description": "",
   "keyword": "Scenario"
 });
 formatter.step({
-  "name": "the User is already logs in to the website",
+  "name": "User is staying at \"login.jsp\"",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_is_already_logs_in_to_the_website()"
+  "location": "TransportNewsManagementFeature.user_is_staying_at(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User clicks the \"sidebar_transferBalance\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"fromOpalID\"",
+  "name": "User has been identified as an admin",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
+  "location": "TransportNewsManagementFeature.user_has_been_identified_as_an_admin()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User choose the \"toOpalID\"",
+  "name": "User smash on the button \"login_submit\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User submit it by clicks on \"transferContinue\"",
+  "name": "User smash on the button \"loginAction_success\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_submit_it_by_clicks_on(String)"
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User choose the \"transferValue\"",
+  "name": "User smash on the button \"News_management\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User submit it by clicks on \"transferNext\"",
+  "name": "User entered all article details",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_submit_it_by_clicks_on(String)"
+  "location": "TransportNewsManagementFeature.user_entered_all_article_details()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User see that the details are correct",
+  "name": "User smash on the button \"createArticle_submit\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_see_that_the_details_are_correct()"
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User clicks on \"confirmTransfer\"",
+  "name": "User smash on the button \"Cucumber Test\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User should see transfer success message",
+  "name": "User entered all update details",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TransportNewsManagementFeature.user_entered_all_update_details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User smash on the button \"updateArticle_update\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User smash on the button \"articleUpdate_back\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User smash on the updated button \"Cucumber Test updated\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TransportNewsManagementFeature.user_smash_on_the_updated_button(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User smash on the button \"updateArticle_delete\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User smash on the button \"articleUpdate_back\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The User should be navigated to \"Create Article\"",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_should_see_transfer_success_message()"
+  "location": "TransportNewsManagementFeature.the_User_should_be_navigated_to(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Successfully edit transfer balance before confirmation",
+  "name": "Customer successfully view the Article",
   "description": "",
   "keyword": "Scenario"
 });
 formatter.step({
-  "name": "the User is already logs in to the website",
+  "name": "User is staying at \"main.jsp\"",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_is_already_logs_in_to_the_website()"
+  "location": "TransportNewsManagementFeature.user_is_staying_at(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User clicks the \"sidebar_transferBalance\"",
+  "name": "User smash on the button \"sidebar_articleView\"",
   "keyword": "When "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_the(String)"
+  "location": "TransportNewsManagementFeature.user_smash_on_the_button(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "the User choose the \"fromOpalID\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"toOpalID\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User submit it by clicks on \"transferContinue\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_submit_it_by_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"transferValue\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User submit it by clicks on \"transferNext\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_submit_it_by_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User see that the details are correct",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_see_that_the_details_are_correct()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"editTransfer\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User edit the transfer balance details",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_edit_the_transfer_balance_details()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"submitEdit\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"confirmTransfer\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User should see transfer success message",
+  "name": "The User should be navigated to \"View Article\"",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "TransferBalanceFeature.the_User_should_see_transfer_success_message()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Successfully cancel transfer balance",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "the User is already logs in to the website",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_is_already_logs_in_to_the_website()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks the \"sidebar_transferBalance\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"fromOpalID\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"toOpalID\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User submit it by clicks on \"transferContinue\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_submit_it_by_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"transferValue\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User submit it by clicks on \"transferNext\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_submit_it_by_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User see that the details are correct",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_see_that_the_details_are_correct()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"cancelTransfer\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User should see cancelled transfer message",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_should_see_cancelled_transfer_message()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Successfully list transfer balance history",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "the User is already logs in to the website",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_is_already_logs_in_to_the_website()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks the \"sidebar_transferBalance\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"showTransferHistory\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User should see the list of transfer balance history",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_should_see_the_list_of_transfer_balance_history()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Successfully search transfer balance history by date",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "the User is already logs in to the website",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_is_already_logs_in_to_the_website()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks the \"sidebar_transferBalance\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"showTransferHistory\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User choose the \"enterDate\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_choose_the(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User clicks on \"searchTransID\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_clicks_on(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the User should see record on the searched date",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "TransferBalanceFeature.the_User_should_see_record_on_the_searched_date()"
+  "location": "TransportNewsManagementFeature.the_User_should_be_navigated_to(String)"
 });
 formatter.result({
   "status": "passed"
