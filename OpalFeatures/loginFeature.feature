64b85edbf22373140a620cfc5c8b690e3d907ef5
kevinrhx <kevinroderick12@gmail.com>
2019-10-17 04:54:00 +1100

    cucumber testing 7

--- OpalFeatures/loginFeature.feature
+++ OpalFeatures/loginFeature.feature
@@ -1,23 +1,23 @@
-# 
-#   Feature: Access Manager
-#  
-#   Scenario: A user will be able to login , and logout
-#       Given the User is in the "main.jsp"
-#       When User click the "navbar_login"
-#       And User fill user information
-#       And User click the "login_submit"
-#       And User click the "loginAction_success"
-#       And User click the "navbar_logout"
-#       Then page should be navigated to "logout"
-#  
-#     
-#   Scenario: A user want to delete their access history 
-#       Given the User is in the "main.jsp"
-#       When User click the "navbar_login"
-#       And User fill user admin information
-#       And User click the "login_submit"
-#       And User click the "loginAction_success"
-#       And User click the "sidebar_AccessManager"
-#       And User click the "delete"
-#       And User refresh page
-#       Then page should be navigated to "Access manager"
+
+  Feature: Access Manager
+ 
+  Scenario: A user will be able to login , and logout
+      Given the User is in the "main.jsp"
+      When User click the "navbar_login"
+      And User fill user information
+      And User click the "login_submit"
+      And User click the "loginAction_success"
+      And User click the "navbar_logout"
+      Then page should be navigated to "logout"
+ 
+    
+  Scenario: A user want to delete their access history 
+      Given the User is in the "main.jsp"
+      When User click the "navbar_login"
+      And User fill user admin information
+      And User click the "login_submit"
+      And User click the "loginAction_success"
+      And User click the "sidebar_AccessManager"
+      And User click the "delete"
+      And User refresh page
+      Then page should be navigated to "Access manager"
--- src/test/java/asd/test/enqiryUnitTest.java
+++ src/test/java/asd/test/enqiryUnitTest.java
@@ -1,71 +1,71 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package asd.test;
-//import asd.model.*;
-//import asd.model.dao.*;
-//import com.mongodb.MongoClient;
-//import com.mongodb.MongoClientURI;
-//import com.mongodb.client.MongoCollection;
-//import com.mongodb.client.MongoDatabase;
-//import static com.mongodb.client.model.Filters.and;
-//import static com.mongodb.client.model.Filters.eq;
-//import java.net.UnknownHostException;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//import org.junit.BeforeClass;
-//import org.bson.Document;
-//import org.junit.AfterClass;
-//
-//
-///**
-// *
-// * @author kevin
-// */
-//public class enqiryUnitTest {
-//     private static MongoDBConnector dbconnector;
-//     public enqiryUnitTest(){
-//     }
-//   
-//     @BeforeClass
-//    public static void setUpClass() throws UnknownHostException {
-//        dbconnector = new MongoDBConnector();
-//        System.out.println("Establish connection to Db");
-//     //   User user = new User("Test","test","test@gmail.com","123","123","098765332","false","");
-//     //   dbconnector.add(user);
-//    }
-//    @Test
-//    public void testAdd ()throws UnknownHostException{
-//        String question = "question";
-//        String answer = "answer";
-//        Enqiry enqiry = new Enqiry("user@gmail.com",question, answer,"1231231","This is The title");
-//        String testOutcome = dbconnector.testAdd(enqiry);
-//        assertEquals("test succeed", testOutcome);
-//        
-//    }
-//    @Test
-//   public void testloadEnqiries() throws UnknownHostException{
-//        //String question = "question";
-//        //String answer = "answer";
-//        //Enqiry enqiry = new Enqiry("user@gmail.com",question, answer,"1231231","This is The title");
-//        String testOutcome = dbconnector.testloadEnqiries();
-//        assertEquals("test succeed", testOutcome);
-//   }
-//     @Test
-//    public void testUpdateEnqiry ()throws UnknownHostException{
-//        String question = "new question";
-//        String answer = "new answer";
-//        Enqiry enqiry = new Enqiry("user@gmail.com",question, answer,"1231231","This is The title");
-//        String testOutcome = dbconnector.testUpdateEnqiry(enqiry);
-//        assertEquals("test succeed", testOutcome);
-//        
-//    }
-//   
-//   
-//   @AfterClass
-//    public static void tearDownClass() {
-//     dbconnector.removeEnqiries("1231231");
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asd.test;
+import asd.model.*;
+import asd.model.dao.*;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import java.net.UnknownHostException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.bson.Document;
+import org.junit.AfterClass;
+
+
+/**
+ *
+ * @author kevin
+ */
+public class enqiryUnitTest {
+     private static MongoDBConnector dbconnector;
+     public enqiryUnitTest(){
+     }
+   
+     @BeforeClass
+    public static void setUpClass() throws UnknownHostException {
+        dbconnector = new MongoDBConnector();
+        System.out.println("Establish connection to Db");
+     //   User user = new User("Test","test","test@gmail.com","123","123","098765332","false","");
+     //   dbconnector.add(user);
+    }
+    @Test
+    public void testAdd ()throws UnknownHostException{
+        String question = "question";
+        String answer = "answer";
+        Enqiry enqiry = new Enqiry("user@gmail.com",question, answer,"1231231","This is The title");
+        String testOutcome = dbconnector.testAdd(enqiry);
+        assertEquals("test succeed", testOutcome);
+        
+    }
+    @Test
+   public void testloadEnqiries() throws UnknownHostException{
+        //String question = "question";
+        //String answer = "answer";
+        //Enqiry enqiry = new Enqiry("user@gmail.com",question, answer,"1231231","This is The title");
+        String testOutcome = dbconnector.testloadEnqiries();
+        assertEquals("test succeed", testOutcome);
+   }
+     @Test
+    public void testUpdateEnqiry ()throws UnknownHostException{
+        String question = "new question";
+        String answer = "new answer";
+        Enqiry enqiry = new Enqiry("user@gmail.com",question, answer,"1231231","This is The title");
+        String testOutcome = dbconnector.testUpdateEnqiry(enqiry);
+        assertEquals("test succeed", testOutcome);
+        
+    }
+   
+   
+   @AfterClass
+    public static void tearDownClass() {
+     dbconnector.removeEnqiries("1231231");
+    }
+}
